8ee17cd26b6d8fdbe4542747090c8b039c9ced4f
radzhiv <radzhivkrishna@gmail.com>
2026-07-06 19:47:36 +0530

    reinitializing repo with new learnings

--- Day7/routes/url.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { handleGenerateNewShortUrl, handleGetAnalytics } = require('../controllers/url')
-
-const router = express.Router()
-
-router.post('/', handleGenerateNewShortUrl)
-router.get('/analytics/:shortId', handleGetAnalytics)
-
-module.exports = router
